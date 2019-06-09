111a445a2e1341e5afbe59e3e26bd572cd3cd1d3
An <annaho1998@gmail.com>
2019-06-09 14:06:50 +0700

    OrdBus

---
+++ WeddingDressManagement/BUS/OrderLineBUS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+using System.Data;
+
+namespace BUS
+{
+    public class OrderLineBUS
+    {
+        OrderLineDAL dal = new OrderLineDAL();
+        public int GetStockQuant (int dressID)
+        {
+            return dal.GetStockQuant(dressID);
+        }
+
+        public int GetDressID(string dressName)
+        {
+            return dal.GetDressID(dressName);
+        }
+
+        public void ReduceStockQuant(int dressID)
+        {
+            dal.ReduceStockQuant(dressID);
+        }
+
+        public DataTable InsertOrderLine(DressDTO dress)
+        {
+            return dal.InsertOrderLine(dress);
+        }
+    }
+}
